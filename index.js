0359088c494933ede28f82e0ec49591e57719374
monz <monz@mainframe2.lan>
2021-08-07 13:56:46 +0100

    create lib and tests

---
+++ index.js
@@ -0,0 +1,54 @@
+
+var os = require('os')
+var fs = require('fs')
+var pull = require('pull-stream')
+var { read, write } = require('pull-files')
+var { createHash } = require('crypto')
+var mkdirp = require('mkdirp')
+
+function sha256sum (input) {
+  var hash = createHash('sha256')
+  hash.update(input)
+  return hash.digest('hex')
+}
+
+function Poirot (files) {
+  if (! (this instanceof Poirot)) return new Poirot (files)
+  if (! Array.isArray(files)) throw Error('files must be an array')
+  this.files = files
+  this.poirot_dir = `${os.homedir()}/.poirot`
+  mkdirp.sync(this.poirot_dir)
+}
+
+Poirot.prototype.sha256sum = sha256sum
+
+Poirot.prototype.update = function (cb) {
+  var self = this
+  pull(
+      read(self.files),
+      pull.through(function (i) {
+	if (!i) return i
+        i.digest = sha256sum(Buffer.from(i.data))
+        if (i.base) i.full_path = i.base+'/'+i.path
+          else i.full_path = i.path
+        delete i.data
+        delete i.path
+        delete i.base
+        return i
+      }),
+      pull.collect(function (err, file) {
+        if (!err) {
+          fs.writeFileSync(
+            self.poirot_dir+'/files.db.json',
+            JSON.stringify(file, void 0, 2)
+          )
+          if (typeof cb === 'function') cb(void 0, true)
+        } else { if (typeof cb === 'function') cb(err); }
+      })
+    )
+}
+
+Poirot.prototype.check  = function () {}
+Poirot.prototype.watch  = function () {}
+
+module.exports = Poirot;
---
+++ test/test.js
@@ -0,0 +1,52 @@
+var fs = require('fs')
+var os = require('os')
+var test = require('tape')
+var Poirot = require('..')
+
+var mock = [
+  `${__dirname}/data/test_1.txt`,
+  `${__dirname}/data/test_2.txt`,
+  `${__dirname}/data/test_3.txt`,
+  `${__dirname}/data/test_4.txt`,
+]
+
+test('can create Poirot', function (t) {
+  t.plan(1)
+  var p = Poirot (mock)
+  t.ok(typeof p === 'object')
+})
+
+test('can import files', function (t) {
+  t.plan(1)
+  var p = Poirot (mock)
+  t.ok(Array.isArray(p.files))
+})
+
+test('can update the files integrity db', function (t) {
+  var p = Poirot (mock)
+  t.plan(p.files.length)
+  p.update()
+  var filesDB = JSON.parse(
+    fs.readFileSync(os.homedir()+'/.poirot/files.db.json').toString()
+  )
+  console.log(filesDB)
+  filesDB.forEach(function (i, index) {
+    var path = mock[index].base+'/'+mock[index].pattern
+    if (i.digest === p.sha256sum(fs.readFileSync(path).toString())) {
+      console.log('imported ', path, ' and integrity check passes')
+      t.ok(true)
+    }
+  })
+})
+
+test('can check the files integrity', function (t) {
+
+
+})
+
+/*
+test('check ', function (t) {
+  t.plan(1)
+  t.ok(true)
+})
+*/
